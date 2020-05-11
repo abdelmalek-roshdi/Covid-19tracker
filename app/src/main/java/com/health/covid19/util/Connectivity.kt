0ce748a111ce4f846f03862af43e82b549d34053
Abd-elmalek <malekroshdi@gmail.com>
2020-05-11 16:11:39 +0200

    added notification icon

--- app/src/main/java/com/health/covid19/util/Connectivity.kt
+++ app/src/main/java/com/health/covid19/util/Connectivity.kt
@@ -3,7 +3,7 @@ package com.health.covid19.util
 import android.app.Application
 import android.content.Context
 import android.net.NetworkInfo
-import javax.inject.Inject
+
 
 class Connectivity(private val app: Application) {
 
